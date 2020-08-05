e009e145eb85d749eb7a4f08e0a01c8feb9b5ee6
Daniel <daniel.lea@nottingham.ac.uk>
2020-08-05 16:52:25 +0100

    created function to close mid point prices from candles request; displayed them as a line graph

--- src/algo_trading_monitor/application.py
+++ src/algo_trading_monitor/application.py
@@ -2,8 +2,51 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+import numpy as np
 import pandas as pd
 
+from configparser import ConfigParser
+from algotradingstuff.accounts import get_account
+from algotradingstuff.sessions import OandaSession
+
+
+def extract_candle_from_response(response):
+    candles = response.get('candles')
+    times = []
+    opens = []
+    highs = []
+    lows = []
+    closes = []
+    for candle in candles:
+        times.append(candle.get('time'))
+        mid_candle = candle.get('mid')
+        opens.append(mid_candle.get('o'))
+        highs.append(mid_candle.get('h'))
+        lows.append(mid_candle.get('l'))
+        closes.append(mid_candle.get('c'))
+    df = pd.DataFrame({'time': times,
+                       'open': opens,
+                       'high': highs,
+                       'low': lows,
+                       'close': closes},
+                      dtype=np.float32)
+    return df
+
+
+parser = ConfigParser()
+parser.read('oanda.txt')
+api_key = parser.get('oanda', 'api_key')
+account_id = parser.get('oanda', 'primary_account')
+base_url = parser.get('oanda', 'base_url')
+
+oanda_account = get_account(account_id, api_key, base_url)
+oanda_session = OandaSession()
+candle_request = oanda_account.get_candles('GBP_USD', granularity='D', count=10)
+candles, lti = oanda_session.send(candle_request)
+print(candles)
+candle_df = extract_candle_from_response(candles)
+print(candle_df.head())
+# exit()
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
@@ -14,13 +57,13 @@ df = pd.DataFrame({
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig = px.line(candle_df, x="time", y="close")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Forex Monitor'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Monitoring close prices for GBP/USD
     '''),
 
     dcc.Graph(
@@ -31,4 +74,4 @@ app.layout = html.Div(children=[
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0')
+    app.run_server(debug=True)#, host='0.0.0.0')
